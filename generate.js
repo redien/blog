45435721ca4b0f4498a73fd7493a59911de0eafb
Jesper Oskarsson <jesosk@gmail.com>
2016-06-20 21:26:34 +0200

    Fixed link in post pages

--- generate.js
+++ generate.js
@@ -9,6 +9,8 @@
 // You should have received a copy of the CC0 Public Domain Dedication along with this software.
 // If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
+var url = 'https://imperfectabstraction.com';
+
 var markdown = require('./markdown/renderer');
 var path = require('path');
 var fs = require('fs');
@@ -72,7 +74,8 @@ var renderFile = function (path) {
         postId: postIdFromPath(path),
         path: path,
         html: markdownHtml,
-        meta: meta
+        meta: meta,
+        url: url
     };
 };
 
